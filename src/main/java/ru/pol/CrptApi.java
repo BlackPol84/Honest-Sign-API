c108f12401a54f56ac865592f4b3fc40988632a8
BlackPol84 <orlovpol84@gmail.com>
2024-06-27 22:04:53 +0300

    deleted the main class and made changes to the CrptApi class

--- src/main/java/ru/pol/CrptApi.java
+++ src/main/java/ru/pol/CrptApi.java
@@ -9,52 +9,50 @@ import java.net.URI;
 import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
+import java.util.List;
 import java.util.concurrent.TimeUnit;
-import java.util.concurrent.locks.ReentrantLock;
+import java.util.concurrent.atomic.AtomicInteger;
+import java.util.concurrent.atomic.AtomicLong;
 
 public class CrptApi {
 
     private final TimeUnit timeUnit;
     private final int requestLimit;
-    private volatile int requestCount;
-    private volatile long lastRequestTime;
-    private final ReentrantLock locker = new ReentrantLock();
+    private AtomicInteger requestCount;
+    private AtomicLong lastRequestTime;
 
     public CrptApi(TimeUnit timeUnit, int requestLimit) {
         this.timeUnit = timeUnit;
         this.requestLimit = requestLimit;
-        this.requestCount = 0;
-        this.lastRequestTime = System.currentTimeMillis();
+        this.requestCount = new AtomicInteger(0);
+        this.lastRequestTime = new AtomicLong(System.currentTimeMillis());
     }
 
-    public void creatDocument(Object document, String signature) throws JsonProcessingException {
+    public void createDocument(Document doc, String signature) throws JsonProcessingException {
 
-        locker.lock();
-        try {
             long currentTime = System.currentTimeMillis();
-            long timeDiff = currentTime - lastRequestTime;
+            long timeDiff = currentTime - lastRequestTime.get();
 
             if (timeDiff >= timeUnit.toMillis(1)) {
-                requestCount = 0;
-                lastRequestTime = currentTime;
+                requestCount.set(0);
+                lastRequestTime.set(currentTime);
             }
 
-            if (requestCount < requestLimit) {
+            if (requestCount.get() < requestLimit) {
+                requestCount.getAndIncrement();
 
                 HttpClient client = HttpClient.newHttpClient();
 
                 HttpRequest request = HttpRequest.newBuilder().
                         uri(URI.create("https://ismp.crpt.ru/api/v3/lk/documents/create")).
-                        POST(HttpRequest.BodyPublishers.ofString(convertToJSON(document))).build();
+                        POST(HttpRequest.BodyPublishers.ofString(convertToJSON(doc))).build();
 
                 try {
                     HttpResponse<String> response = client.send(request, HttpResponse.BodyHandlers.ofString());
                     System.out.println("The status of the response obtained: " + response.statusCode());
 
-                    requestCount++;
-
                 } catch (IOException | InterruptedException e) {
-                    e.getStackTrace();
+                    throw new RuntimeException(e);
                 }
             } else {
                 try {
@@ -63,13 +61,63 @@ public class CrptApi {
                     throw new RuntimeException(e);
                 }
             }
-        } finally {
-            locker.unlock();
-        }
     }
 
-    private String convertToJSON(Object document) throws JsonProcessingException {
+    private String convertToJSON(Document doc) throws JsonProcessingException {
         ObjectWriter ow = new ObjectMapper().writer().withDefaultPrettyPrinter();
-        return ow.writeValueAsString(document);
+        return ow.writeValueAsString(doc);
+    }
+
+    public static void main(String[] args) {
+
+        CrptApi api = new CrptApi(TimeUnit.SECONDS, 10);
+    }
+
+    public static class Document {
+        private Description description;
+        private String doc_id;
+        private String doc_status;
+        private String doc_type;
+        private boolean importRequest;
+        private String owner_inn;
+        private String participant_inn;
+        private String producer_inn;
+        private String production_date;
+        private String production_type;
+        private List<Product> products;
+        private String reg_date;
+        private String reg_number;
+
+        // getters and setters
+    }
+
+    public static class Product {
+        private String certificate_document;
+        private String certificate_document_date;
+        private String certificate_document_number;
+        private String owner_inn;
+        private String producer_inn;
+        private String production_date;
+        private String tnved_code;
+        private String uit_code;
+        private String uitu_code;
+
+        // getters and setters
+    }
+
+    public static class Description {
+        private String participantInn;
+
+        public Description(String participantInn) {
+            this.participantInn = participantInn;
+        }
+
+        public String getParticipantInn() {
+            return participantInn;
+        }
+
+        public void setParticipantInn(String participantInn) {
+            this.participantInn = participantInn;
+        }
     }
 }
--- src/main/java/ru/pol/Main.java
+++
@@ -1,7 +0,0 @@
-package ru.pol;
-
-public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
-    }
-}
